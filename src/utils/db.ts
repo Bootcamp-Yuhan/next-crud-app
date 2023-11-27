1aaa07f59b26af0793c1c02d78d2cd8c515e38ee
yuhan <yuhan.adit@gmail.com>
2023-11-27 23:04:52 +0700

    Database Setup

---
+++ src/utils/db.ts
@@ -0,0 +1,12 @@
+// utils/db.ts
+import { Pool } from 'pg';
+
+const pool = new Pool({
+  user: process.env.PGSQL_USER,
+  host: process.env.PGSQL_HOST,
+  database: process.env.PGSQL_DATABASE,
+  password: process.env.PGSQL_PASSWORD,
+  port: 5432
+});
+
+export default pool;
