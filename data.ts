009960a1028ae30d5091ae54a15030efd482b4be
yuhan <yuhan.adit@gmail.com>
2023-11-27 21:26:31 +0700

    Add Category Data

---
+++ data.ts
@@ -0,0 +1,33 @@
+export const categoryData = [
+    {
+      "id": 1,
+      "initial": "ABC",
+      "name": "Barang ABC",
+      "active": true,
+      "created_by": "Bambang Sugeni",
+      "created_on": 0,
+      "modified_by": null,
+      "modified_on": null
+    },
+    {
+      "id": 2,
+      "initial": "DEF",
+      "name": "Barang DEF",
+      "active": true,
+      "created_by": "Bambang Sugeni",
+      "created_on": 0,
+      "modified_by": null,
+      "modified_on": null
+    },
+    {
+      "id": 3,
+      "initial": "GHI",
+      "name": "Barang GHI",
+      "active": true,
+      "created_by": "Bambang Sugeni",
+      "created_on": 0,
+      "modified_by": null,
+      "modified_on": null
+    }
+  ];
+  
